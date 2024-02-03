74ed44b6d4043b005fc966cc60b66d01b4510bc0
Ace De Castro <acetdecastro@gmail.com>
2024-02-03 21:35:30 +0800

    Add ChatList UI

---
+++ src/components/chat-list/ChatList.tsx
@@ -0,0 +1,45 @@
+import List from "@mui/material/List";
+import ChatListItem from "./chat-list-item/ChatListItem";
+import ChatListHeader from "./chat-list-header/ChatListHeader";
+import { Divider, Stack } from "@mui/material";
+
+const ChatList = () => {
+  return (
+    <Stack>
+      <ChatListHeader />
+      <Divider />
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+          maxHeight: "80vh",
+          overflow: "auto",
+        }}
+      >
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+      </List>
+    </Stack>
+  );
+};
+
+export default ChatList;
