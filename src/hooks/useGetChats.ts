10db8cad2e78956d39ec2d8bc1c1e08777ab0188
Ace De Castro <acetdecastro@gmail.com>
2024-02-05 18:38:22 +0800

    Add useGetChat

--- src/hooks/useGetChats.ts
+++ src/hooks/useGetChats.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@apollo/client";
 import { graphql } from "../gql";
 
-export const getChatsDocument = graphql(`
+const getChatsDocument = graphql(`
   query Chats {
     chats {
       ...ChatFragment
